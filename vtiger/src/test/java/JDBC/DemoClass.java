8706fed33cd5be13f45fd76f254517ad7e5a0a86
AMAN RAJ <AMAN RAJ@LAPTOP-UD0GH7J4>
2024-02-26 22:58:19 +0530

    creata a rep

--- vtiger/src/test/java/JDBC/DemoClass.java
+++ vtiger/src/test/java/JDBC/DemoClass.java
@@ -5,7 +5,8 @@ public class DemoClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("this is main method");
+		System.out.println("this is main method1");
+		System.out.println("this is main method2");
 			
 		}
 	}
